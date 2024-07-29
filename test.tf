350feda4ba48c41139c97bc145a36c3a6038425f
Adil Touati <adiltouati@microsoft.com>
2024-07-29 09:19:27 -0400

    init

---
+++ test.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "my_rg" {
+  name     = "gjhdgdhjgdh"
+  location = "East US"
+  tags = {
+    "Owner"       = "Bo Katan Kryze"
+    "Cost Center" = "Mandalor Core"
+
+  }
+}
