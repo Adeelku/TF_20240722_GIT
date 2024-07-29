2afeaafa416244898f3f5e01e00d50febb4a28d1
Adil Touati <adiltouati@microsoft.com>
2024-07-29 17:19:16 -0400

    fic be

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "tfstare-rg"
+    resource_group_name = "tfstate-rg"
     key = "state-dev"
     storage_account_name = "vddvghdvghddhg12"
     container_name = "tfstate"
